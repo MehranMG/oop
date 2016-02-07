10365a18b0f2cff12ce85afafa338bc68df91f1d
lmadhuranga <lilan.maduranga@gmail.com>
2016-02-07 23:35:37 +0530

    cart model complete

--- views/items.php
+++ views/items.php
@@ -2,6 +2,6 @@
 include '../model/items_model.php';
 
 $items_model = new Items_model();
-var_dump($items_model->get_all()); exit("  | <b>".'items_file ln:'.__LINE__."</b>");
-var_dump(''); exit("  | <b>".'items_file ln:'.__LINE__."</b>");
+
+
  ?>
